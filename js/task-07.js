a55b70b8817cb5f6c2e3be1013c10ba43a660bcc
Svin07 <svin07@ukr.net>
2023-07-07 13:17:39 +0300

    DZ_7_2

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,9 @@
 const inputRef = document.getElementById("font-size-control");
 const textRef = document.getElementById("text");
+inputRef.value = "16";
+
+// console.dir(inputRef);
 inputRef.addEventListener("input", function () {
-    textRef.style.fontSize = inputRef.value + "px";
+   
+    textRef.style.fontSize = inputRef.value  + "px";
 });
